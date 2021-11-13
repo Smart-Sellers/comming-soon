1e610c325d41cc06e6cc88b4168768ef2c82c151
Manuk <manuk.minasyan1@gmail.com>
2021-11-13 18:07:09 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Timer from "./components/Countdown/Timer";
+import Timer from "../components/Countdown/Timer";
 
 export default function Home() {
   return (
